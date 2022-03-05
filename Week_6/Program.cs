26739a361b6cc394b86706914fd5fe502e24e469
Michael Cardoso <50937149+Mikecar26@users.noreply.github.com>
2022-03-05 13:18:21 -0600

    initial commit

---
+++ Week_6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace program
+{
+    public class Program
+    {
+        static void Main()
+        {
+            ProblemOne();
+            ProblemTwo();
+        }
+        static void ProblemOne()
+        {
+            List<string> input = new List<string>();
+
+            Console.WriteLine("Enter the names of the person who liked your photo. Otherwise press enter.");
+            while (true)
+            {
+                string name = Console.ReadLine();
+                if (name == "")
+                    break;
+                input.Add(name);
+            }
+            if (input.Count == 0)
+                Console.WriteLine("");
+            else if (input.Count == 1)
+                Console.Write(input);
+            else if (input.Count == 2)
+            {
+                Console.Write(input[0] + " and " + input[1]);
+            }
+            else
+            {
+                Console.Write(input[0] + ", " + input[1] + " and " + (input.Count() - 2) + " others");
+            }
+            Console.Write(" liked your post");
+        }
+        static void ProblemTwo()
+        {
+            Dictionary<char, int> histograph = new Dictionary<char, int>();
+            Console.WriteLine("Enter a sentence:");
+            string input = Console.ReadLine();
+
+            foreach (char c in input)
+            {
+                if (c == ' ')
+                    continue;
+                if (histograph.ContainsKey(c))
+                    histograph[c] += 1;
+                else
+                    histograph.Add(c, 1);
+            }
+            foreach (KeyValuePair<char,int> i in histograph)
+                Console.WriteLine(i);
+        }
+    }
+}
